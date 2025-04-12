002752f10cd788a25e72593d55348ceef5caf58b
Alex Simoneau <afsimoneau@gmail.com>
2025-04-12 16:18:41 -0400

    remove unused js

--- src/js/main.js
+++
@@ -1,9 +0,0 @@
-import { createNavigation } from './components/navigation.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-  // Find the nav element and replace it with our navigation component
-  const existingNav = document.querySelector('nav');
-  if (existingNav) {
-    existingNav.replaceWith(createNavigation());
-  }
-});
